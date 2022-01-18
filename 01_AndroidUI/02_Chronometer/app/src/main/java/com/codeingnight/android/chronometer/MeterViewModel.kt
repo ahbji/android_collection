3d50c64e7c895afe7465f5b7eba01eff3aa80aff
andreaji <andrea.hb.ji@outlook.com>
2022-01-18 13:16:11 +0800

    提交 Chronometer

---
+++ 01_AndroidUI/02_Chronometer/app/src/main/java/com/codeingnight/android/chronometer/MeterViewModel.kt
@@ -0,0 +1,28 @@
+package com.codeingnight.android.chronometer
+
+import android.os.SystemClock
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class MeterViewModel(val handle: SavedStateHandle) : ViewModel() {
+    private val ELAPSED_TIME_KEY = "ELAPSED_TIME"
+
+    init {
+        if (!handle.contains(ELAPSED_TIME_KEY)) {
+            handle.set(ELAPSED_TIME_KEY, 0)
+        }
+    }
+
+    fun getElapsedTime(): MutableLiveData<Long> {
+        return handle.getLiveData(ELAPSED_TIME_KEY)
+    }
+
+    fun setElapsedTime(base: Long) {
+        handle.set(ELAPSED_TIME_KEY, SystemClock.elapsedRealtime() - base)
+    }
+
+    fun getBase(): Long {
+        return SystemClock.elapsedRealtime() - getElapsedTime().value!!
+    }
+}
